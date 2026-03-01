c61b6fd235af8d4449eaf0f77f54b8dec5f98861
bikash-backendcraftsman <bikash.backendcraftsman@gmail.com>
2026-03-01 17:24:11 +0530

    git add docs/abstraction-coffee-example.md

--- src/main/java/com/technostack/lld/ops/abstraction/CoffeMachine.java
+++ src/main/java/com/technostack/lld/ops/abstraction/CoffeMachine.java
@@ -1,4 +1,43 @@
 package com.technostack.lld.ops.abstraction;
 
 public class CoffeMachine {
+
+    private String machineModelNum;
+    private String brand;
+    private int price;
+
+    public CoffeMachine() {
+
+    }
+
+    public CoffeMachine(String machineModelNum, String brand, int price) {
+        this.machineModelNum = machineModelNum;
+        this.brand = brand;
+        this.price = price;
+    }
+
+    private void heatWater() {
+        System.out.println("Heating water...");
+    }
+
+    private void grindBeans() {
+        System.out.println("Grinding beans...");
+    }
+
+    private void brew() {
+        System.out.println("Brewing coffee...");
+    }
+
+    public void prepareCoffee(){
+        heatWater();
+        grindBeans();
+        brew();
+        System.out.println("Coffee is ready");
+    }
+
+    public static void main(String[] args) {
+        CoffeMachine machine = new CoffeMachine("CF-0090","Breson Coffee House",2200);
+        machine.prepareCoffee();
+
+    }
 }
--- src/main/java/com/technostack/lld/ops/abstraction/CoffeeVendorSupplier.java
+++ src/main/java/com/technostack/lld/ops/abstraction/CoffeeVendorSupplier.java
@@ -1,4 +1,14 @@
 package com.technostack.lld.ops.abstraction;
 
 public class CoffeeVendorSupplier {
+    public static void main(String[] args) {
+        CoffeMachine machine = new CoffeMachine();
+        machine.prepareCoffee();
+
+        /*
+        We cat call this method as part of abstration, because ita private method,
+         can only be accessible inside the same class it has been written.
+         */
+       // machine.heatWater();
+    }
 }
